ad0857754d198ed44e65d7289a2a31c0ae41bd1e
daniel konforty <danielkon42@gmail.com>
2025-05-29 16:24:05 +0300

    made some cleanup changes

--- server/db/db.py
+++
@@ -1,6 +0,0 @@
-from mongoDB import MongoDB
-from utils import uri, db_name, users_collection_name
-
-if __name__ == "__main__":
-    db = MongoDB(uri, db_name, users_collection_name)
-    print(db.insert_user("tyrone", "123456"))
